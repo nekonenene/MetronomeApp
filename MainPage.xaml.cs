3980da702e62a364ff33f22d45234bad6d26eb0f
nekonenene <hatonekoe@gmail.com>
2023-05-05 15:37:33 +0900

    ボタンを押して音声が鳴るように

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
+using System.Xml.Serialization;
 using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
 
 namespace MetronomeApp;
 
@@ -6,12 +8,15 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
     private readonly ILogger logger;
+    private readonly IAudioManager audioManager;
+    private IAudioPlayer audioPlayer;
 
-	public MainPage(ILoggerFactory loggerFactory)
+    public MainPage(ILoggerFactory loggerFactory, IAudioManager audioManager)
 	{
 		InitializeComponent();
 
         logger = loggerFactory.CreateLogger<MainPage>();
+        this.audioManager = audioManager;
     }
 
 	private void OnCounterClicked(object sender, EventArgs e)
@@ -26,11 +31,23 @@ public partial class MainPage : ContentPage
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 
+    private async void PlaySound()
+    {
+        if (audioPlayer == null)
+        {
+            audioPlayer = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("move_cursor1.mp3"));
+        }
+
+        audioPlayer.Play();
+    }
+
     private void OnPlaySoundButtonClicked(object sender, EventArgs e)
     {
         logger.LogDebug("aaaDebug");
         logger.LogInformation("aaaInfo");
         logger.LogWarning("aaaWarn");
         logger.LogError("aaaError");
+
+        PlaySound();
     }
 }
