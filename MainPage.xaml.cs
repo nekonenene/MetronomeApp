d4edf877d70c166269745a69d1be411ac52c548c
nekonenene <hatonekoe@gmail.com>
2023-05-05 21:35:52 +0900

    元のカウンターを消して丸４つを並べた

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -25,15 +25,4 @@ public partial class MainPage : ContentPage {
 
         _logger.LogInformation("End of init MainPage");
     }
-
-    private void OnCounterClicked(object sender, EventArgs e) {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
 }
